aff4aeacad0f105a6c41ff836ce380b306b37286
MONU PAL <monupal@192.168.1.28>
2025-09-17 01:24:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+//console.log("Hello");
+console.log('hello');
+
+// variable and const 
+var a = 10;
+var b = 30;
+const c = 45;
+
+var e = 10;
+var f = 30;
+const d = 45;
+
+var g = 10;
+var h = 30;
+const i = 45;
+
+// Two Types of data types 
+ // Primitive  
+ // Refference 
+
+// 1.  Primitive Types of data types 
+// Monu - String 
+// 12 - integer
+// 12.5 - float
+// True - Boolean
+// a - character
+// "jsdhnfwr48n" - String 
+
+// 2. Refference data types 
+// {}
+// []
+// ()
+
+// loops
+//1.  for loops 
+ // for (start; end; change){
+ // }
+// eg 1. for ( var = 1; i < 6; i++){
+//}
+
+
+
+ // 2. while lopps
+ // start;
+ // while(end){
+ // change;
+ //}
